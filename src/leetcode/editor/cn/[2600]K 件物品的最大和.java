a162b251f6b517028e067ba49ebfbca8467396db
呙启伟 <2657944563@qq.com>
2023-07-05 14:14:47 +0800

    leetcode练习-6.18

---
+++ src/leetcode/editor/cn/[2600]K 件物品的最大和.java
@@ -0,0 +1,19 @@
+/*
+K 件物品的最大和
+2600
+2023-07-05 13:36:04
+*/
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+
+        if (k <= numOnes) {
+            return k;
+        } else if (k < numOnes + numZeros) {
+            return numOnes;
+        } else {
+            return numOnes - k + numOnes + numZeros;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
