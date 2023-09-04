2615839f6daa2b9a72adb33b21b4d68e72544d4c
呙启伟 <2657944563@qq.com>
2023-09-04 14:54:31 +0800

    :sparkles: 51

---
+++ src/leetcode/editor/cn/[51]N 皇后.java
@@ -0,0 +1,78 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+N 皇后
+51
+2023-09-04 11:40:41
+*/
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    List<List<String>> re;
+
+    public List<List<String>> solveNQueens(int n) {
+        re = new LinkedList<>();
+        char[][] result = new char[n][n];
+        for (int i = 0; i < result.length; i++) {
+            Arrays.fill(result[i], '.');
+        }
+        rac(result, 0);
+        return re;
+    }
+
+    // n 行 m 列
+    void rac(char[][] result, int n) {
+        if (n == result.length) {
+            add(result);
+            return;
+        }
+        for (int m = 0; m < result.length; m++) {
+            if (verify(result, n, m)) {
+                result[n][m] = 'Q';
+                rac(result, n + 1);
+                result[n][m] = '.';
+            }
+        }
+    }
+
+    // n 行 m 列
+    boolean verify(char[][] result, int n, int m) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < result.length; j++) {
+                if (result[i][j] == 'Q') {
+                    // 同一个
+                    if (i == n && m == j) {
+                        continue;
+                    }
+                    // 同一斜线
+                    if (i - j == n - m) {
+                        return false;
+                    }
+                    if (i + j == n + m) {
+                        return false;
+                    }
+                    // 同一行
+                    if (i == n) {
+                        return false;
+                    }
+                    // 同一列
+                    if (j == m) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    void add(char[][] result) {
+        LinkedList<String> s = new LinkedList<>();
+        re.add(s);
+        for (int i = 0; i < result.length; i++) {
+            s.add(String.valueOf(result[i]));
+        }
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
