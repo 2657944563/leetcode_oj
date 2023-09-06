4c58823640fd5d4811a564feac73de8ddfb3df1f
呙启伟 <2657944563@qq.com>
2023-09-06 23:31:48 +0800

    :sparkles: 342

---
+++ src/leetcode/editor/cn/[292]Nim 游戏.java
@@ -0,0 +1,12 @@
+/*
+Nim 游戏
+292
+2023-09-06 23:13:32
+*/
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
