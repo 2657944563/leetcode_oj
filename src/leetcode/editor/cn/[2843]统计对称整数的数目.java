b6680087aa3c40e3b646e991b91cac9e74bce3b1
呙启伟 <2657944563@qq.com>
2023-09-11 18:10:00 +0800

    :sparkles: 2843

--- src/leetcode/editor/cn/[2843]统计对称整数的数目.java
+++ src/leetcode/editor/cn/[2843]统计对称整数的数目.java
@@ -6,7 +6,30 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
+        int cnt = 0;
+        // 暴力法：枚举可能出现的数字
+        int[] possibleNum = new int[]{11, 22, 33, 44, 55, 66, 77, 88, 99};
+        for (int n : possibleNum) {
+            if (n >= low && n <= high) {
+                cnt++;
+            }
+        }
 
+        if (high < 1000) {
+            return cnt;
+        }
+        if (low < 1000) {
+            low = 1000;
+        }
+
+        for (int i = low; i <= high; i++) {
+            String s = String.valueOf(i);
+            if (s.charAt(0) + s.charAt(1) == s.charAt(2) + s.charAt(3)) {
+                cnt++;
+            }
+        }
+        return cnt;
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
