1875afabb977d31e705dbb5598c27e3a92f212ff
呙启伟 <2657944563@qq.com>
2023-09-13 18:13:17 +0800

    :sparkles:1423

--- src/leetcode/editor/cn/[1423]可获得的最大点数.java
+++ src/leetcode/editor/cn/[1423]可获得的最大点数.java
@@ -1,27 +1,23 @@
-import java.util.LinkedList;
-import java.util.List;
-
 /*
 可获得的最大点数
 1423
 2023-09-13 17:10:17
 */
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int maxScore(int[] cardPoints, int k) {
-        List<Integer> list = new LinkedList<>();
-        int n = cardPoints.length - k, result = 0, min = Integer.MAX_VALUE, tmin = 0;
-        for (int i = 0; i < cardPoints.length; i++) {
-            result += cardPoints[i];
-            tmin = i + n <= cardPoints.length ? 0 : tmin;
-            for (int j = i; j < i + n && i + n <= cardPoints.length; j++) {
-                tmin += cardPoints[j];
-                list.add(cardPoints[j]);
-            }
-            min = Math.min(min, tmin);
-        }
-        return result - min;
-    }
 
+class Solution {
+    public int maxScore(int[] nums, int k) {
+        int n = nums.length, len = n - k;
+        int sum = 0, cur = 0;
+        for (int i = 0; i < n; i++) sum += nums[i];
+        for (int i = 0; i < len; i++) cur += nums[i];
+        int min = cur;
+        for (int i = len; i < n; i++) {
+            cur = cur + nums[i] - nums[i - len];
+            min = Math.min(min, cur);
+        }
+        return sum - min;
+    }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
