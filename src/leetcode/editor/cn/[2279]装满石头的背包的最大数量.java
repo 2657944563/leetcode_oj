305d177d1f574f9f9414854b713c20019c16bd2a
呙启伟 <2657944563@qq.com>
2023-09-19 17:04:02 +0800

    :sparkles:2279

--- src/leetcode/editor/cn/[2279]装满石头的背包的最大数量.java
+++ src/leetcode/editor/cn/[2279]装满石头的背包的最大数量.java
@@ -6,7 +6,18 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-
+        int n = capacity.length, res = 0;
+        for (int i = 0; i < n; i++) {
+            capacity[i] -= rocks[i];
+        }
+        Arrays.sort(capacity);
+        for (int i = 0; i < n; i++) {
+            additionalRocks -= capacity[i];
+            if (additionalRocks < 0) break;
+            res++;
+        }
+        return res;
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
