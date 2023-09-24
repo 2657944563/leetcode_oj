98c9675681f8a2ecbcf33c5d5ae36971a66adabd
呙启伟 <2657944563@qq.com>
2023-09-24 09:28:27 +0800

    :sparkles:704

---
+++ src/leetcode/editor/cn/[704]二分查找.java
@@ -0,0 +1,55 @@
+// 开始时间：2023-09-24 09:25:18
+// 题目标题：二分查找
+// 题目标记：binary-search
+// 题目编号：704
+// 题目描述：
+
+/**
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返
+ * 回 -1。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * <p>
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ * <p>
+ * <p>
+ * Related Topics 数组 二分查找 👍 1448 👎 0
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, mid;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
