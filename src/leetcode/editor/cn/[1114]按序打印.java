42cd493ec623613f707c4480ab1198a0f3bbab29
呙启伟 <2657944563@qq.com>
2023-09-28 14:15:05 +0800

    :sparkles: 1114

--- src/leetcode/editor/cn/[1114]按序打印.java
+++ src/leetcode/editor/cn/[1114]按序打印.java
@@ -103,6 +103,37 @@ class Foo {
         LockSupport.park();
         printThird.run();
     }
+    /* 8+ms
+
+    Semaphore s2 = new Semaphore(0);
+    Semaphore s3 = new Semaphore(0);
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        s2.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        s2.acquire();
+        printSecond.run();
+        s3.release();
+
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+        s3.acquire();
+        printThird.run();
+    }
+     */
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
