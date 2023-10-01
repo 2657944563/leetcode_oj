9d6f075f81d43b6745d796bc7fd37e0f5659ac5b
呙启伟 <2657944563@qq.com>
2023-10-01 23:03:11 +0800

    :sparkles: 61,123

---
+++ src/leetcode/editor/cn/[61]旋转链表.java
@@ -0,0 +1,92 @@
+// 开始时间：2023-09-30 15:09:21
+// 题目标题：旋转链表
+// 题目标记：rotate-list
+// 题目编号：61
+// 题目描述：
+/**
+ * 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[4,5,1,2,3]
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [0,1,2], k = 4
+ * 输出：[2,0,1]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 链表中节点的数目在范围 [0, 500] 内
+ * -100 <= Node.val <= 100
+ * 0 <= k <= 2 * 10⁹
+ * <p>
+ * <p>
+ * Related Topics 链表 双指针 👍 988 👎 0
+ * <p>
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+        ListNode temp = head;
+        int len = 1;
+        while (temp.next != null) {
+            len += 1;
+            temp = temp.next;
+        }
+        temp.next = head;
+        k = k % len;
+        for (int i = 0; i < len - k; i++) {
+            temp = temp.next;
+        }
+        head = temp.next;
+        temp.next = null;
+        return head;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
