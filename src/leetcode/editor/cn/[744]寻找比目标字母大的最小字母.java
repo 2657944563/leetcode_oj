e890ae4a6d1cd51b1600c445573e8c58a4c41f0a
呙启伟 <2657944563@qq.com>
2023-10-03 15:08:33 +0800

    :sparkles: 744

--- src/leetcode/editor/cn/[744]寻找比目标字母大的最小字母.java
+++ src/leetcode/editor/cn/[744]寻找比目标字母大的最小字母.java
@@ -49,8 +49,16 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-
+        int n = letters.length;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (letters[mid] > target) r = mid;
+            else l = mid + 1;
+        }
+        return letters[r] > target ? letters[r] : letters[0];
     }
 }
+
 //leetcode submit region end(Prohibit modification and deletion)
 
