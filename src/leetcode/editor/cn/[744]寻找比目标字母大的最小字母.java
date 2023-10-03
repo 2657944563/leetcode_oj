4142872b0d7403fc4aaae7c7c5ba12e1ba5aaf61
呙启伟 <2657944563@qq.com>
2023-10-03 14:59:43 +0800

    :sparkles: 744

---
+++ src/leetcode/editor/cn/[744]寻找比目标字母大的最小字母.java
@@ -0,0 +1,56 @@
+// 开始时间：2023-10-03 12:14:46
+// 题目标题：寻找比目标字母大的最小字母
+// 题目标记：find-smallest-letter-greater-than-target
+// 题目编号：744
+// 题目描述：
+
+/**
+ * 给你一个字符数组 letters，该数组按非递减顺序排序，以及一个字符 target。letters 里至少有两个不同的字符。
+ * <p>
+ * 返回 letters 中大于 target 的最小的字符。如果不存在这样的字符，则返回 letters 的第一个字符。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入: letters = ["c", "f", "j"]，target = "a"
+ * 输出: "c"
+ * 解释：letters 中字典上比 'a' 大的最小字符是 'c'。
+ * <p>
+ * 示例 2:
+ * <p>
+ * <p>
+ * 输入: letters = ["c","f","j"], target = "c"
+ * 输出: "f"
+ * 解释：letters 中字典顺序上大于 'c' 的最小字符是 'f'。
+ * <p>
+ * 示例 3:
+ * <p>
+ * <p>
+ * 输入: letters = ["x","x","y","y"], target = "z"
+ * 输出: "x"
+ * 解释：letters 中没有一个字符在字典上大于 'z'，所以我们返回 letters[0]。
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 2 <= letters.length <= 10⁴
+ * letters[i] 是一个小写字母
+ * letters 按非递减顺序排序
+ * letters 最少包含两个不同的字母
+ * target 是一个小写字母
+ * <p>
+ * <p>
+ * Related Topics 数组 二分查找 👍 284 👎 0
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
