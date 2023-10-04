fe7425a1874a70fc713436ded4dc1ca30a251a4d
呙启伟 <2657944563@qq.com>
2023-10-04 14:41:02 +0800

    :sparkles: 188,705

---
+++ src/leetcode/editor/cn/[188]买卖股票的最佳时机 IV.java
@@ -0,0 +1,75 @@
+// 开始时间：2023-10-04 12:30:22
+// 题目标题：买卖股票的最佳时机 IV
+// 题目标记：best-time-to-buy-and-sell-stock-iv
+// 题目编号：188
+// 题目描述：
+
+/**
+ * 给你一个整数数组 prices 和一个整数 k ，其中 prices[i] 是某支给定的股票在第 i 天的价格。
+ * <p>
+ * 设计一个算法来计算你所能获取的最大利润。你最多可以完成 k 笔交易。也就是说，你最多可以买 k 次，卖 k 次。
+ * <p>
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：k = 2, prices = [2,4,1]
+ * 输出：2
+ * 解释：在第 1 天 (股票价格 = 2) 的时候买入，在第 2 天 (股票价格 = 4) 的时候卖出，这笔交易所能获得利润 = 4-2 = 2 。
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：k = 2, prices = [3,2,6,5,0,3]
+ * 输出：7
+ * 解释：在第 2 天 (股票价格 = 2) 的时候买入，在第 3 天 (股票价格 = 6) 的时候卖出, 这笔交易所能获得利润 = 6-2 = 4 。
+ * 随后，在第 5 天 (股票价格 = 0) 的时候买入，在第 6 天 (股票价格 = 3) 的时候卖出, 这笔交易所能获得利润 = 3-0 = 3 。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 1 <= k <= 100
+ * 1 <= prices.length <= 1000
+ * 0 <= prices[i] <= 1000
+ * <p>
+ * <p>
+ * Related Topics 数组 动态规划 👍 1059 👎 0
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int n = prices.length;
+        k = Math.min(k, n / 2);
+        int[][] buy = new int[n][k + 1];
+        int[][] sell = new int[n][k + 1];
+
+        buy[0][0] = -prices[0];
+        sell[0][0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            buy[0][i] = sell[0][i] = Integer.MIN_VALUE / 2;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            buy[i][0] = Math.max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+            for (int j = 1; j <= k; ++j) {
+                buy[i][j] = Math.max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+                sell[i][j] = Math.max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);
+            }
+        }
+
+        return Arrays.stream(sell[n - 1]).max().getAsInt();
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+
---
+++ src/leetcode/editor/cn/[705]设计哈希集合.java
@@ -0,0 +1,133 @@
+// 开始时间：2023-10-03 15:08:46
+// 题目标题：设计哈希集合
+// 题目标记：design-hashset
+// 题目编号：705
+// 题目描述：
+
+import java.util.LinkedList;
+
+/**
+ * 不使用任何内建的哈希表库设计一个哈希集合（HashSet）。
+ * <p>
+ * 实现 MyHashSet 类：
+ * <p>
+ * <p>
+ * void add(key) 向哈希集合中插入值 key 。
+ * bool contains(key) 返回哈希集合中是否存在这个值 key 。
+ * void remove(key) 将给定值 key 从哈希集合中删除。如果哈希集合中没有这个值，什么也不做。
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * <p>
+ * 输入：
+ * ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove",
+ * "contains"]
+ * [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+ * 输出：
+ * [null, null, null, true, false, null, true, null, false]
+ * <p>
+ * 解释：
+ * MyHashSet myHashSet = new MyHashSet();
+ * myHashSet.add(1);      // set = [1]
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(1); // 返回 True
+ * myHashSet.contains(3); // 返回 False ，（未找到）
+ * myHashSet.add(2);      // set = [1, 2]
+ * myHashSet.contains(2); // 返回 True
+ * myHashSet.remove(2);   // set = [1]
+ * myHashSet.contains(2); // 返回 False ，（已移除）
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 0 <= key <= 10⁶
+ * 最多调用 10⁴ 次 add、remove 和 contains
+ * <p>
+ * <p>
+ * Related Topics 设计 数组 哈希表 链表 哈希函数 👍 311 👎 0
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyHashSet {
+    LinkedList<Integer>[] hash;
+    int size;
+    int count;
+
+    public MyHashSet() {
+        size = 16;
+        count = 0;
+        hash = new LinkedList[size];
+    }
+
+    public void add(int key) {
+        if (count == size) {
+            resize();
+        }
+        int h = key % size;
+        LinkedList<Integer> integers = hash[h];
+        if (integers == null) {
+            LinkedList<Integer> newList = new LinkedList<>();
+            newList.add(key);
+            hash[h] = newList;
+        } else {
+            if (!integers.contains(key)) {
+                integers.add(key);
+            }
+        }
+
+    }
+
+    public void remove(int key) {
+        int h = key % size;
+        LinkedList<Integer> integers = hash[h];
+        if (integers != null && integers.contains(key)) {
+            integers.remove((Integer) key);
+        }
+    }
+
+    public boolean contains(int key) {
+        int h = key % size;
+        LinkedList<Integer> integers = hash[h];
+        if (integers != null) {
+            return integers.contains(key);
+        }
+        return false;
+    }
+
+    void resize() {
+        size *= 2;
+        LinkedList<Integer>[] newHash = new LinkedList[size];
+        reIndex(newHash);
+        hash = newHash;
+    }
+
+    void reIndex(LinkedList<Integer>[] newHash) {
+        int h;
+        for (LinkedList<Integer> integers : hash) {
+            for (Integer integer : integers) {
+                h = integer % size;
+                LinkedList<Integer> list = newHash[h];
+                if (list == null) {
+                    LinkedList<Integer> newList = new LinkedList<>();
+                    newList.add(integer);
+                    newHash[h] = newList;
+                } else {
+                    list.add(h);
+                }
+            }
+        }
+
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
