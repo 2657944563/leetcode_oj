8eed57d055f44779dc7d3415cae788d271530f78
呙启伟 <2657944563@qq.com>
2023-10-08 15:50:45 +0800

    :sparkles: 876

---
+++ src/leetcode/editor/cn/[876]链表的中间结点.java
@@ -0,0 +1,80 @@
+// 开始时间：2023-10-07 15:35:45
+// 题目标题：链表的中间结点
+// 题目标记：middle-of-the-linked-list
+// 题目编号：876
+// 题目描述：
+/**
+ * 给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+ * <p>
+ * 如果有两个中间结点，则返回第二个中间结点。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[3,4,5]
+ * 解释：链表只有一个中间结点，值为 3 。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5,6]
+ * 输出：[4,5,6]
+ * 解释：该链表有两个中间结点，值分别为 3 和 4 ，返回第二个结点。
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * <p>
+ * 链表的结点数范围是 [1, 100]
+ * 1 <= Node.val <= 100
+ * <p>
+ * <p>
+ * Related Topics 链表 双指针 👍 946 👎 0
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            temp = temp.next.next;
+            head = head.next;
+        }
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
